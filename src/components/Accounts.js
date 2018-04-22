a1be51b364008aa199bb5365268f68450dc6ed58
Christian Hamburger Grøngaard <christianhg@gmail.com>
2018-04-22 17:01:18 +0200

    WIP

--- src/components/Accounts.js
+++ src/components/Accounts.js
@@ -1,45 +1,63 @@
-import React from 'react'
 import Link from 'gatsby-link'
+import React from 'react'
+import styled from 'styled-components'
 
 const accounts = [
   {
     link: 'https://flickr.com/photos/christianhg',
     name: 'flickr',
-    username: 'christianhg',
   },
   {
     link: 'https://github.com/christianhg',
     name: 'github',
-    username: 'christianhg',
   },
   {
     link: 'https://last.fm/user/christianhg',
     name: 'last.fm',
-    username: 'christianhg',
   },
   {
     link: 'https://www.linkedin.com/in/christianhg',
     name: 'linkedin',
-    username: 'christianhg',
   },
   {
     link: 'https://twitter.com/christianhg',
     name: 'twitter',
-    username: 'christianhg',
   },
 ]
 
+const Ol = styled.ol`
+  /* display: inline-flex; */
+  padding-left: 0.5em;
+  border-left: 1px solid #222222;
+`
+
+const Li = styled.li`
+  list-style: none;
+  position: relative;
+  padding-left: 15px;
+  &:before {
+    content: "";
+    display: block;
+    position: absolute;
+    width: 5px;
+    height: 5px;
+    top: 2px;
+    left: -3px;
+    background: #222222;
+  }
+  /* padding-right: 1em; */
+`
+
 const Account = account => (
-  <li key={account.link}>
+  <Li key={account.link}>
     <a href={account.link} rel="me">
-      {account.name}/<strong>{account.username}</strong>
+      {account.name}
     </a>
-  </li>
+  </Li>
 )
 
 export const Accounts = () => (
   <section>
-    <h2>Elsewhere</h2>
-    <ol>{accounts.map(Account)}</ol>
+    <Ol>{accounts.map(Account)}</Ol>
   </section>
 )
