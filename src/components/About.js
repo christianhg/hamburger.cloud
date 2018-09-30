e16c722eaa903923dd30ee995b28eedbef85676b
Christian Hamburger Grøngaard <christianhg@gmail.com>
2018-09-30 11:33:31 +0200

    Updage about section

--- src/components/About.js
+++ src/components/About.js
@@ -2,16 +2,22 @@ import React from 'react'
 
 export const About = () => (
   <section>
-    <h1>Christian Hamburger Grøngaard</h1>
-    <ul>
-      <li>Functional programming enthusiast.</li>
-      <li>
-        Highly interested in the craft of programming and creating simpler
-        programs.
-      </li>
-      <li>
-        Front-End Developer at <a href="http://escenic.com">Escenic AS</a>.
-      </li>
-    </ul>
+    <h1>Hi, I'm Christian</h1>
+    <p>
+      I'm a functional programming enthusiast, highly interested in the craft of
+      programming and creating simpler programs.
+    </p>
+    <p>
+      I currently work as a front-end developer at{' '}
+      <a href="http://escenic.com">Escenic AS</a> where I fight complexity in
+      large applications for newsrooms. I've been developing for the Web on and
+      off since the early 2000s, I got a good eye for design and UX and used to
+      pursue a more design-oriented career.
+    </p>
+    <p>
+      When I don't find myself occupied by doing, reading or thinking about
+      programming, I'm enjoying my time together with my family in the wonderful
+      city of Oslo, Norway.
+    </p>
   </section>
 )
