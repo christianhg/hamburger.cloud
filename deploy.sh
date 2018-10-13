a61c76a0b2c7154507e91209604dffe24aeb328b
Christian Hamburger Grøngaard <christianhg@gmail.com>
2018-10-13 20:33:17 +0200

    Replace hard-coded strings with variables

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ set -x
 
 tar -czf $ARTIFACT $DIST_DIR && \
 scp $ARTIFACT $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
-ssh $REMOTE_USER@$REMOTE_HOST 'cd /var/www/hamburger.cloud && tar zxvf public.tgz -C .'
+ssh $REMOTE_USER@$REMOTE_HOST "cd $REMOTE_APP_DIR && tar zxvf $ARTIFACT -C ."
