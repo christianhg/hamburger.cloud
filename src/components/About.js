c8bb9985b0be0ade87072fd521b44f3758d9f61f
Christian Hamburger Grøngaard <christianhg@gmail.com>
2019-02-07 20:33:52 +0100

    Fix h1 size on front page

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 export const About = () => (
-  <section>
+  <>
     <h1>Hi, I'm Christian</h1>
     <p>
       I’m a passionate programmer, highly interested in the craft of programming
       and the challenges that arise when programming becomes collaborative.
     </p>
-  </section>
+  </>
 );
