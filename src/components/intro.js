b05526d87739d7fc6a47be1eec818296e2c70fea
Christian Hamburger Grøngaard <christianhg@gmail.com>
2019-02-08 21:29:56 +0100

    Bang!

--- src/components/intro.js
+++ src/components/intro.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Intro = () => (
   <section id="intro">
     <div className="inner">
-      <h1>Hi, I'm Christian</h1>
+      <h1>Hi, I'm Christian!</h1>
       <p>
         I’m a passionate programmer, highly interested in the craft of
         programming and the challenges that arise when programming becomes
