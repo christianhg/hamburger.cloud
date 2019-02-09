e73ddaff3e9338c0cee63b669b09d1912788d85c
Christian Hamburger Grøngaard <christianhg@gmail.com>
2019-02-09 19:37:11 +0100

    Move query to articles list

--- src/components/ArticlesList.js
+++ src/components/ArticlesList.js
@@ -1,27 +1,52 @@
+import { graphql, StaticQuery } from 'gatsby';
 import { Link } from 'gatsby';
 import React from 'react';
 
-export const ArticlesList = ({ articles }) => (
-  <section id="article-list">
-    <div className="inner">
-      <h2>Writings</h2>
-      <ol className="article-list">
-        {articles.map(article => (
-          <li className="article-list-item" key={article.id}>
-            <span className="flip-vertically">
-              <Link to={article.path}>{article.title}</Link>
-              <span className="article-date">{article.date}</span>
-            </span>
-          </li>
-        ))}
-      </ol>
-    </div>
-  </section>
-);
-
-export const nodeToArticle = ({ node }) => ({
+const nodeToArticle = ({ node }) => ({
   date: node.frontmatter.date,
   id: node.id,
   path: node.frontmatter.path,
   title: node.frontmatter.title,
 });
+
+export const ArticlesList = () => (
+  <StaticQuery
+    query={graphql`
+      query ArticlesListQuery {
+        allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+          edges {
+            node {
+              id
+              frontmatter {
+                title
+                date(formatString: "MMMM DD, YYYY")
+                path
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => {
+      const articles = data.allMarkdownRemark.edges.map(nodeToArticle);
+
+      return (
+        <section id="article-list">
+          <div className="inner">
+            <h2>Writings</h2>
+            <ol className="article-list">
+              {articles.map(article => (
+                <li className="article-list-item" key={article.id}>
+                  <span className="flip-vertically">
+                    <Link to={article.path}>{article.title}</Link>
+                    <span className="article-date">{article.date}</span>
+                  </span>
+                </li>
+              ))}
+            </ol>
+          </div>
+        </section>
+      );
+    }}
+  />
+);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,47 +1,24 @@
-import { graphql, StaticQuery } from 'gatsby';
 import React from 'react';
 import { Intro } from '../components/intro';
-import { ArticlesList, nodeToArticle } from '../components/ArticlesList';
+import { ArticlesList } from '../components/ArticlesList';
 import About from '../components/about';
 import { Header } from '../components/Header';
 import Layout from '../components/layout';
 import { Footer } from '../components/footer';
 
 export default () => (
-  <StaticQuery
-    query={graphql`
-      query IndexQuery {
-        allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
-          edges {
-            node {
-              id
-              frontmatter {
-                title
-                date(formatString: "MMMM DD, YYYY")
-                path
-              }
-            }
-          }
-        }
-      }
-    `}
-    render={data => (
-      <Layout
-        config={{
-          title: 'Personal website of Christian Hamburger Grøngaard',
-          description: 'Personal website of Christian Hamburger Grøngaard',
-          keywords: 'hamburger, cloud, christian, grøngaard',
-          path: '/',
-        }}
-      >
-        <Header />
-        <Intro />
-        <ArticlesList
-          articles={data.allMarkdownRemark.edges.map(nodeToArticle)}
-        />
-        <About />
-        <Footer />
-      </Layout>
-    )}
-  />
+  <Layout
+    config={{
+      title: 'Personal website of Christian Hamburger Grøngaard',
+      description: 'Personal website of Christian Hamburger Grøngaard',
+      keywords: 'hamburger, cloud, christian, grøngaard',
+      path: '/',
+    }}
+  >
+    <Header />
+    <Intro />
+    <ArticlesList />
+    <About />
+    <Footer />
+  </Layout>
 );
