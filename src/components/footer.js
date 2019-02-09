10082bfdfe3b902c8cf45d2388c03912417c835d
Christian Hamburger Grøngaard <christianhg@gmail.com>
2019-02-09 02:30:38 +0100

    Add copyright

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,10 +3,15 @@ import React from 'react';
 export const Footer = () => (
   <footer>
     <div className="inner">
-      <a href="https://github.com/christianhg/hamburger.cloud">
-        {'{'}src{'}'}
-      </a>
-      <a href="/rss.xml">RSS</a>
+      <span className="copyright">
+        Copyright © {new Date().getFullYear()} Christian Hamburger Grøngaard
+      </span>
+      <span className="misc">
+        <a href="https://github.com/christianhg/hamburger.cloud">
+          {'{'}src{'}'}
+        </a>
+        <a href="/rss.xml">RSS</a>
+      </span>
     </div>
   </footer>
 );
