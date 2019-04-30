46f06e33a889108290e3acdbf0ff7228c4f8cb6f
Christian Hamburger Grøngaard <christianhg@gmail.com>
2019-04-30 19:58:10 +0200

    Move top border to header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,18 @@
 import { Link } from 'gatsby';
 import React from 'react';
+import styled from 'styled-components';
 import logo from './logo.svg';
 
+const HeaderInner = styled.header`
+  border-top: 0.25rem solid #772873;
+`;
+
 export const Header = () => (
-  <header>
+  <HeaderInner>
     <div className="inner">
       <Link className="logo" to="/">
         <img src={logo} alt="Christians 'G' logo" />
       </Link>
     </div>
-  </header>
+  </HeaderInner>
 );
