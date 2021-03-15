3593102c2eb94c3c49532c6773c2f520e576c8be
Christian Hamburger Grøngaard <christianhg@gmail.com>
2021-03-15 20:40:16 +0100

    Remove unused code

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-set -x
-
-tar -czf $ARTIFACT $DIST_DIR && \
-scp $ARTIFACT $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
-ssh $REMOTE_USER@$REMOTE_HOST "cd $REMOTE_APP_DIR && tar zxvf $ARTIFACT -C ."
